cfdf316ddec80df83c212469c8ab525689434872
Øyvind Samuelsen <samu.developments@gmail.com>
2024-07-29 16:26:19 +0200

    move pluginManagement repositories block

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -7,3 +7,16 @@ dependencyResolutionManagement {
       }
    }
 }
+
+pluginManagement {
+   repositories {
+      mavenLocal()
+      mavenCentral()
+      maven {
+         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+      }
+      maven {
+         url = uri("https://plugins.gradle.org/m2/")
+      }
+   }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,5 @@
 rootProject.name = "hoplite"
 
-pluginManagement {
-   repositories {
-      mavenLocal()
-      mavenCentral()
-      maven {
-         url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-      }
-      maven {
-         url = uri("https://plugins.gradle.org/m2/")
-      }
-   }
-}
-
 include(
    ":hoplite-core",
    ":hoplite-azure",
